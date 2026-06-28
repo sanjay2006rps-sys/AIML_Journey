1a3fd97cf3cb64c91618063948ef94359b6056d7
sanjay2006rps-sys <sanjay2006rps@gmail.com>
2026-06-28 13:57:19 +0530

    Complete Python Module 1 exercises

---
+++ Python/Module_01/exc.1.py
@@ -0,0 +1,30 @@
+#exc.1
+name="Sanjay"
+age=19
+favourite_book="Atomic Habit"
+favourite_programming_language="python"
+print(f"My name is {name},i am {age} years old,my favourite book is {favourite_book},my favourite programming language is {favourite_programming_language}")
+#exc.2
+name=input()
+age=int(input())
+print(f"Hello {name},you will be {age} years old")
+#exc.3
+a=int(input())
+b=int(input())
+print(a+b,a-b,a*b,a/b,a%b,a**b,a//b)
+#exc.4
+weight=float(input())
+height=float(input())
+BMI = weight / (height ** 2)
+print(BMI)
+#exc.5
+name=input()
+age=input()
+college=input()
+dept=input()
+cgpa=float(input())
+print(f"name:{name}")
+print(f"age:{age}")
+print(f"college:{college}")
+print(f"department:{dept}")
+print(f"CGPA:{cgpa}")
